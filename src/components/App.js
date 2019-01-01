ffbe299c459d14caa74c28bdf9d186435872827d
Farida <faridah.absi@gmail.com>
2019-01-01 22:10:25 +0000

    Automatically backed up by Learn

--- src/components/App.js
+++ src/components/App.js
@@ -42,7 +42,7 @@ class App extends Component {
         fruit={this.state.fruit}
         filters={this.state.filters}
         currentFilter={this.state.currentFilter}
-        onUpdateFilter={this.updateFilter} 
+        onUpdateFilter={this.updateFilter}
       />
     );
   }
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,20 +1,15 @@
 import React from 'react';
 
-
-const Filter = (props) => {
-  return (
-    <select onChange={props.handleChange} defaultValue='all'>
-      <option value='all'>All</option>
-      {props.filters.map(filter =>
-        <option key={filter} value={filter}>{filter}</option>
-      )}
-    </select>
-  );
-}
-
+const Filter = ({ filters, handleChange }) => 
+  <select onChange={handleChange} defaultValue='all'>
+    <option value='all'>All</option>
+    {filters.map(filter =>
+      <option key={filter} value={filter}>{filter}</option>
+    )}
+  </select>;
 Filter.defaultProps = {
   filters: [],
-  handleChange: undefined
-}
+  handleChange: function() {}
+};
 
 export default Filter;
