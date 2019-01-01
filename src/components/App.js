553477214ae44d26e23be4dcc83016cfc9d677bc
Farida <faridah.absi@gmail.com>
2019-01-01 22:10:06 +0000

    Automatically backed up by Learn

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import FruitBasket from './FruitBasket';
 
-class App extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			fruit: [],
-			filters: [],
-			currentFilter: null
-		}
-	}
+// const App = () => <FruitBasket />;
 
-	componentWillMount() {
-		this.fetchFruit();
-    	this.fetchFilters();
-  	}
+class App extends Component {
+  constructor() {
+    super();
 
-  	fetchFilters = () => {
-	    fetch('/api/fruit_types')
-	    .then(response => response.json())
-	    .then(filters => this.setState({ filters }));
-  	}
+    this.state = {
+      currentFilter: null,
+      filters: [],
+      fruit: []
+    };
+  }
 
-	fetchFruit = () => {
-		fetch('/api/fruit')
-      	.then(response => response.json())
-      	.then(fruit => this.setState({ fruit }))
-	}
+  componentDidMount() {
+    this.fetchFilters();
+    this.fetchFruit();
+  }
 
-	updateFilter= (event) => {
-		console.log('new filter: ', event.target.value);
-    	this.setState({ currentFilter: event.target.value });
-	}
+  fetchFilters = () => {
+    fetch('/api/fruit_types')
+      .then(response => response.json())
+      .then(filters => this.setState({ filters }));
+  }
 
-	render() {
-		return(<FruitBasket fruit={this.state.fruit}
-			filters={this.state.filters}
-			currentFilter={this.state.currentFilter}
-			updateFilterCallback={this.updateFilter} />)
-	}
+  fetchFruit = () => {
+    fetch('/api/fruit')
+      .then(response => response.json())
+      .then(fruit => this.setState({ fruit }));
+  }
+
+  updateFilter = event => {
+    console.log('update filter to: ', event.target.value);
+    this.setState({currentFilter: event.target.value });
+  }
+
+  render() {
+    return (
+      <FruitBasket
+        fruit={this.state.fruit}
+        filters={this.state.filters}
+        currentFilter={this.state.currentFilter}
+        onUpdateFilter={this.updateFilter} 
+      />
+    );
+  }
 }
 
 export default App;
